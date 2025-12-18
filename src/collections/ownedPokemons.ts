1e3b8f87a29857e09cce5cf25ae65c5e7e31fda8
eduverdu7 <eduverduo@gmail.com>
2025-12-18 15:10:07 -0500

    Final

--- src/collections/ownedPokemons.ts
+++ src/collections/ownedPokemons.ts
@@ -1,5 +1,6 @@
 import { ObjectId } from "mongodb";
 import { getDB } from "../db/mongo";
+import { COLLECTION_OWNED, COLLECTION_TRAINERS, COLLECTION_POKEMONS} from "../utils";
 
 const rand = () => Math.floor(Math.random() * 100) + 1;
 
@@ -10,37 +11,75 @@ export const catchPokemon = async (
 ) => {
   const db = getDB();
 
-  const res = await db.collection("ownedPokemons").insertOne({
-    trainerId: new ObjectId(trainerId),
-    pokemonId: new ObjectId(pokemonId),
+  const trainer = await db
+    .collection(COLLECTION_TRAINERS)
+    .findOne({ _id: new ObjectId(trainerId) });
+
+  if (!trainer) {
+    throw new Error("Trainer not found");
+  }
+
+  if (trainer.pokemons.length >= 6) {
+    throw new Error("Trainer already has 6 pokemons");
+  }
+
+  const pokemonExists = await db
+    .collection(COLLECTION_POKEMONS)
+    .findOne({ _id: new ObjectId(pokemonId) });
+
+  if (!pokemonExists) {
+    throw new Error("Pokemon not found");
+  }
+
+  const ownedResult = await db.collection(COLLECTION_OWNED).insertOne({
+    pokemon: pokemonId, 
     nickname,
     attack: rand(),
     defense: rand(),
     speed: rand(),
     special: rand(),
-    level: rand()
+    level: 1
   });
 
-  return db
-    .collection("ownedPokemons")
-    .findOne({ _id: res.insertedId });
+  await db.collection(COLLECTION_TRAINERS).updateOne(
+    { _id: new ObjectId(trainerId) },
+    { $addToSet: { pokemons: ownedResult.insertedId.toString() } }
+  );
+
+  return db.collection(COLLECTION_OWNED).findOne({
+    _id: ownedResult.insertedId
+  });
 };
+
 
 export const freePokemon = async (
   ownedPokemonId: string,
   trainerId: string
 ) => {
   const db = getDB();
-  const ownedId = new ObjectId(ownedPokemonId);
 
-  const owned = await db
-    .collection("ownedPokemons")
-    .findOne({ _id: ownedId });
+  const trainer = await db
+    .collection(COLLECTION_TRAINERS)
+    .findOne({ _id: new ObjectId(trainerId) });
 
-  if (!owned || owned.trainerId.toString() !== trainerId)
-    throw new Error("Not allowed");
+  if (!trainer) {
+    throw new Error("No se ha encontrado entrenador");
+  }
 
-  await db.collection("ownedPokemons").deleteOne({ _id: ownedId });
+  if (!trainer.pokemons.includes(ownedPokemonId)) {
+    throw new Error("Pokemon no pertenece a entrenador");
+  }
 
-  return owned;
+  await db.collection(COLLECTION_TRAINERS).updateOne(
+    { _id: new ObjectId(trainerId) },
+    { $pull: { pokemons: ownedPokemonId } as any}
+  );
+
+  await db.collection(COLLECTION_OWNED).deleteOne({
+    _id: new ObjectId(ownedPokemonId)
+  });
+
+  return db.collection(COLLECTION_TRAINERS).findOne({
+    _id: new ObjectId(trainerId)
+  });
 };
--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,27 +1,20 @@
 import { Db, MongoClient } from "mongodb";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 let client: MongoClient;
-let dB: Db;
-const dbName = "Vicio";
+let db: Db;
 
 export const connectToMongoDB = async () => {
-    try{
-        const mongoUrl = process.env.MONGO_URL;
-        if(mongoUrl){
-            client = new MongoClient(mongoUrl);
-            await client.connect();
-            dB = client.db(dbName);
-            console.log("Estás conectado al mongo");
-        } else {
-            throw new Error("MONGO_URL is not defined in environment variables");
-        }
-    }
-    catch(err){
-        console.log("Error del mongo: ", err)
-    }
+  const mongoUrl = process.env.MONGO_URL;
+  if (!mongoUrl) throw new Error("MONGO_URL not defined");
+
+  client = new MongoClient(mongoUrl);
+  await client.connect();
+  db = client.db();
+
+  console.log("Estás conectado al mondongo cosa guapa!");
 };
 
-export const getDB = ():Db => dB;
+export const getDB = (): Db => db;
